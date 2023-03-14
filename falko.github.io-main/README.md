2f6618ce547d04ee0be12d66faaaa73f21fdfaab
falkoweb <126210855+falkoweb@users.noreply.github.com>
2023-03-14 23:59:14 +0100

    Add files via upload

---
+++ falko.github.io-main/README.md
@@ -0,0 +1,1 @@
+Link: https://falkoweb.github.io/
---
+++ falko.github.io-main/public/js/transition_page.js
@@ -0,0 +1,74 @@
+content_page1 = [
+    `<h3><strong>Falko Saltman</strong></h3><br><br><br><img src="public/img/Apparence.png" style="width: auto;height: 75%;" alt="">`,
+    
+    `
+        <h3><strong>Rapport :</strong></h3>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <img
+            src="public/img/1.png"
+            class="lazyload"
+            alt=""
+            style="max-width: 100%; height: auto; display: block; margin: 0 auto; box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);"
+            width="732"
+            height="494"
+            data-sizes="auto"
+        >
+
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <button onclick="PageSuivante(1)">Page Précedente</button></div>
+    `,
+
+    `<button onclick="PageSuivante(2)">Page Précedente</button></div>
+    `,
+];
+content_page2 = [
+    `<h3><strong>Identité</strong></h3>
+    <br>
+    <p>Nom : Saltman</p>
+    <p>Prenom : Falko</p>
+    <br>
+    <p>Lieux de Naissance : Shiganshina</p>
+    <p>Date de Naissance : 22 Avril 768</p>
+    <p>Age : 20 ans</p><br><p>Lieux de Résidence : Shiganshina</p>
+    <p>Evenement Notable :</p>
+    <dd> - Réussi à rejoindre les brigades spéciales</dd>
+    <br>
+    <br>
+    <br>
+    <br>
+    <img style="width: auto; height: 30%;"  src="public/img/Tampon-BS.jpg" alt="">
+    <div class="flex-box"><button onclick="PageSuivante(2)">Page Suivante</button></div>`,
+    `
+    <h3><strong>13 Février 788</strong></h3>
+    <h3>Objet : Patrouille et garde de la caserne en sous-effectif</h3>
+    <p>En ce jour, d'après les ordres du SGT-C Gunther Genzo. Nous avons dû sécuriser la caserne en l'absence de la garnison. J'ai été en binôme avec le 2nd Classe Codd Hellsteiner.</p> 
+
+    <h3><strong>14 Février 788</strong></h3>
+        <h3>Objet : Enquête sur un potentiel cadet asiatique</h3>
+        <p>Aujourd’hui, avec le 2nd Classe Codd Hellsteiner, on à entendu parler d'un cadet nommé Rin qui serait asiatique. Donc nous sommes partis au camp d'entrainement pour questionner les instructeurs. Un instructeur avoue connaitre l'individu. Cependant l'instructeur Ludwig nie les faits, pourtant un cadet présent à coté de l'instructeur à avoué que Rin était présent dans les rangs. En inspectant l'individu, j'ai pu en conclure qu'il n'était pas asiatique.</p> 
+        <button onclick="PageSuivante(3)">Page Suivante</button></div>
+    `,
+
+    `
+    
+    `,
+];
+
+function PageSuivante(nb) {
+    document.getElementById("page1").innerHTML = content_page1[nb - 1];
+    document.getElementById("page2").innerHTML = content_page2[nb - 1];
+};
+
+setTimeout(function() {
+    PageSuivante(1);
+}, 10);
